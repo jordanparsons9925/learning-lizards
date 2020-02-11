429e187c670642da75b10715fff0b6acff76b1e9
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2020-02-10 23:26:34 -0330

    completed brain class

--- Assets/Scripts/memoryScript.cs
+++ Assets/Scripts/memoryScript.cs
@@ -6,78 +6,64 @@ using UnityEngine.SceneManagement;
 public static class memoryScript
 {
     public class Brain {
-        public Brain()
-        {
-            public IDictionary<string, int[,]> parentA = new Dictionary<string, int[,]>();
-            public IDictionary<string, int[,]> parentB = new Dictionary<string, int[,]>();
-            public IDictionary<string, int[,]> childA = new Dictionary<string, int[,]>();
-            public IDictionary<string, int[,]> childB = new Dictionary<string, int[,]>();
+        public Brain() {
+            IDictionary<string, int[,]> parentA = new Dictionary<string, int[,]>();
+            IDictionary<string, int[,]> parentB = new Dictionary<string, int[,]>();
+            IDictionary<string, int[,]> childA = new Dictionary<string, int[,]>();
+            IDictionary<string, int[,]> childB = new Dictionary<string, int[,]>();
+            float scoreA = 0.0f;
+            float scoreB = 0.0f;
         }
 
-        
-    }
-    //Parents
-    static public IDictionary<string, int[,]> parentBrain1A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain2A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain3A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain4A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain5A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain6A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain7A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain8A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain9A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain10A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain1B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain2B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain3B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain4B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain5B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain6B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain7B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain8B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain9B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> parentBrain10B = new Dictionary<string, int[,]>();
+        public void queueParents() {
+            parentA.Clear();
+            foreach (string currentBehaviour in childA.Keys) {
+                int[,] currentArray = childA[currentBehaviour];
+                parentA.Add(currentBehaviour, new int[4,2]);
+                for (int x = 0; x <= 4; x++) {
+                    for (int y = 0; y <=2; y++) {
+                        parentA[currentBehaviour][x,y] = currentArray[x,y];
+                    }
+                }
+            }
 
-    //Children
-    static public IDictionary<string, int[,]> bestChild1A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild2A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild3A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild4A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild5A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild6A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild7A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild8A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild9A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild10A = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild1B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild2B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild3B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild4B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild5B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild6B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild7B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild8B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild9B = new Dictionary<string, int[,]>();
-    static public IDictionary<string, int[,]> bestChild10B = new Dictionary<string, int[,]>();
+            parentB.Clear();
+            foreach (string currentBehaviour in childB.Keys) {
+                int[,] currentArray = childB[currentBehaviour];
+                parentB.Add(currentBehaviour, new int[4,2]);
+                for (int x = 0; x <= 4; x++) {
+                    for (int y = 0; y <=2; y++) {
+                        parentB[currentBehaviour][x,y] = currentArray[x,y];
+                    }
+                }
+            }
+            scoreA = 0.0f;
+            scoreB = 0.0f;
+        }
+    }
+
+    Brain family1 = new Brain();
+    Brain family2 = new Brain();
+    Brain family3 = new Brain();
+    Brain family4 = new Brain();
+    Brain family5 = new Brain();
+    Brain family6 = new Brain();
+    Brain family7 = new Brain();
+    Brain family8 = new Brain();
+    Brain family9 = new Brain();
+    Brain family10 = new Brain();
 
     static public int generation;
     static public int family;
 
-    static public float generationalBestA;
-    static public float generationalBestB;
-
     static memoryScript() {
         generation = 1;
         family = 1;
-        generationalBestA = 0.0f;
-        generationalBestB = 0.0f;
     }
 
     static void nextFamily() {
         if (family <= 10) {
             family++;
-        generationalBestA = 0.0f;
-        generationalBestB = 0.0f;
         }
         nextGeneration();
     }
@@ -85,18 +71,15 @@ public static class memoryScript
     static void nextGeneration() {
         generation++;
         family = 0;
-        generationalBestA = 0.0f;
-        generationalBestB = 0.0f;
-        
-        parentBrain1A.Clear();
-        foreach (string currentBehaviour in bestChild1A.Keys) {
-            int[,] currentArray = bestChild1A[currentBehaviour];
-            parentBrain1A.Add(currentBehaviour, new int[4,2]);
-            for (int x = 0; x <= 4; x++) {
-                for (int y = 0; y <=2; y++) {
-                    parentBrain1A[currentBehaviour][x,y] = currentArray[x,y];
-                }
-            }
-        }
+        family1.queueParents();
+        family2.queueParents();
+        family3.queueParents();
+        family4.queueParents();
+        family5.queueParents();
+        family6.queueParents();
+        family7.queueParents();
+        family8.queueParents();
+        family9.queueParents();
+        family10.queueParents();
     }
 }
