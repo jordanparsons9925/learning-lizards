a61cc45873cf0cb78847a02859ddf187a510a636
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2020-02-17 14:16:04 -0330

    improved camera movement

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class cameraFollow : MonoBehaviour
 {
     public Transform farTracker;
+
+    private int cameraShot;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        cameraShot = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(farTracker);
+
+        if (cameraShot == 0) {
+            transform.position = new Vector3(-121.8f, 44, 44.7f);
+            Debug.Log(transform.rotation.y);
+            if (transform.rotation.y < 0.45f)
+                cameraShot++;
+        } else if (cameraShot == 1) {
+            transform.position = new Vector3(-70, 20, 287.4f);
+        }
     }
 }
